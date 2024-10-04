75535928e50fddfff0ab1676cdf6d578b1b921e3
EddieSparrow <Ed26rus@yandex.ru>
2024-10-04 15:10:22 +0300

    feat: create half of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.scss';
+import './styles/header.scss';
+import './styles/menu.scss';
+import './styles/about.scss';
+import './styles/learning.scss';
+import './styles/regions.scss';
+import './styles/everybody.scss';
 
 function App() {
   const [count, setCount] = useState(0);
