4bbaa8687d3915fbe7d705bebe55cfc1d603eda5
EddieSparrow <Ed26rus@yandex.ru>
2024-10-11 15:04:53 +0300

    feat: add anchors and slider for about section and maps focus fills

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,62 @@ import App from './App.tsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{<App />}</React.StrictMode>);
+
+const focusButtons = document.querySelectorAll('.button-container-1, .button-container-2, .button-container-3');
+const modal = document.querySelector('.regions__modal');
+const mapContainer = document.querySelectorAll('.regions__map-1, .regions__map-2, .regions__map-3, .regions__map-4');
+const defaultMapFill = 'rgba(174, 207, 240, 0.3019607843)';
+const flys = document.querySelector('.regions__flys');
+
+focusButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    modal.style.right = '-0.0156vw';
+    flys.style.opacity = 0;
+  });
+});
+
+focusButtons[0].addEventListener('click', () => {
+  mapContainer.forEach((map) => {
+    map.style.fill = 'rgba(254, 230, 41, 1)';
+  });
+});
+
+focusButtons[1].addEventListener('click', () => {
+  mapContainer.forEach((map) => {
+    map.style.fill = 'rgba(227, 63, 0, 1)';
+  });
+});
+
+focusButtons[2].addEventListener('click', () => {
+  mapContainer.forEach((map) => {
+    map.style.fill = 'rgba(115, 204, 227, 1)';
+  });
+});
+
+document.addEventListener('click', (event) => {
+  if (!modal.contains(event.target) && !Array.from(focusButtons).some((button) => button.contains(event.target))) {
+    modal.style.right = '-58.0156vw';
+    flys.style.opacity = 1;
+    mapContainer.forEach((map) => {
+      map.style.fill = defaultMapFill;
+    });
+  }
+});
+
+const imageContainer = document.querySelector('.about__image-wrapper');
+const circleAbout = document.querySelector('.circle.about');
+let curreentTranslate = 0;
+let countTranslate = 0;
+
+circleAbout.addEventListener('click', () => {
+  countTranslate += 1;
+  if (countTranslate === 1) {
+    curreentTranslate -= 42.4375;
+  } else if (countTranslate === 2) {
+    curreentTranslate -= 43.4375;
+  } else {
+    curreentTranslate = 2;
+    countTranslate = 0;
+  }
+  imageContainer.style.transform = `translateX(${curreentTranslate}vw)`;
+});
