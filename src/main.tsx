80f7cedc29167fc53e6e1df74187a4f231c366ed
EddieSparrow <Ed26rus@yandex.ru>
2024-10-13 22:27:33 +0300

    feat: add map points

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import './styles/index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{<App />}</React.StrictMode>);
 
 const focusButtons = document.querySelectorAll('.button-container-1, .button-container-2, .button-container-3');
+const mapPoints = document.querySelectorAll('.map-point');
+const mapPointContainer = document.querySelector('.regions__points');
 const modal = document.querySelector('.regions__modal');
 const mapContainer = document.querySelectorAll('.regions__map-1, .regions__map-2, .regions__map-3, .regions__map-4');
 const defaultMapFill = 'rgba(174, 207, 240, 0.3019607843)';
@@ -13,8 +15,10 @@ const flys = document.querySelector('.regions__flys');
 
 focusButtons.forEach((button) => {
   button.addEventListener('click', () => {
+    hideAllPoints();
     modal.style.right = '-0.0156vw';
     flys.style.opacity = 0;
+    mapPointContainer.style.opacity = 1;
   });
 });
 
@@ -22,11 +26,17 @@ focusButtons[0].addEventListener('click', () => {
   mapContainer.forEach((map) => {
     map.style.fill = 'rgba(254, 230, 41, 1)';
   });
+  mapPoints.forEach((map) => {
+    map.style.fill = 'rgba(227, 63, 0, 1)';
+  });
 });
 
 focusButtons[1].addEventListener('click', () => {
   mapContainer.forEach((map) => {
     map.style.fill = 'rgba(227, 63, 0, 1)';
+  });
+  mapPoints.forEach((map) => {
+    map.style.fill = 'rgba(115, 204, 227, 1)';
   });
 });
 
@@ -34,16 +44,41 @@ focusButtons[2].addEventListener('click', () => {
   mapContainer.forEach((map) => {
     map.style.fill = 'rgba(115, 204, 227, 1)';
   });
+  mapPoints.forEach((map) => {
+    map.style.fill = 'rgba(254, 230, 41, 1)';
+  });
 });
 
 document.addEventListener('click', (event) => {
   if (!modal.contains(event.target) && !Array.from(focusButtons).some((button) => button.contains(event.target))) {
+    hideAllPoints();
     modal.style.right = '-58.0156vw';
     flys.style.opacity = 1;
+    mapPointContainer.style.opacity = 0;
     mapContainer.forEach((map) => {
       map.style.fill = defaultMapFill;
     });
   }
+});
+
+function hideAllPoints() {
+  document.querySelectorAll('.map-point').forEach((point) => {
+    point.style.opacity = 0;
+  });
+}
+
+const buttonCity1 = document.querySelector('.modal-city-1');
+const pointCity1 = document.querySelector('.map-point-1');
+buttonCity1?.addEventListener('click', () => {
+  hideAllPoints();
+  pointCity1.style.opacity = 1;
+});
+
+const buttonCity2 = document.querySelector('.modal-city-2');
+const pointCity2 = document.querySelector('.map-point-2');
+buttonCity2?.addEventListener('click', () => {
+  hideAllPoints();
+  pointCity2.style.opacity = 1;
 });
 
 const imageContainer = document.querySelector('.about__image-wrapper');
