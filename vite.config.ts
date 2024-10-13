c5b330ee21e295346c5d71dc2974e223504c0b9c
EddieSparrow <Ed26rus@yandex.ru>
2024-10-13 22:57:00 +0300

    feat: change build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/public/secondPage.html',
+          dest: '',
+          rename: 'secondPage.html',
+        },
+        {
+          src: 'src/public/testPage.html',
+          dest: '',
+          rename: 'testPage.html',
+        },
+      ],
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@import "./src/styles/index.css";`,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'secondPage.html' || assetInfo.name === 'testPage.html') {
+            return 'pages/[name].[ext]';
+          }
+          return 'assets/[name].[ext]';
+        },
       },
     },
   },
